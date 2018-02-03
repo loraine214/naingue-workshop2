f8b2fddab16033d11ca6afb79bd6a28a9465f943
loraine214 <35474962+loraine214@users.noreply.github.com>
2018-02-03 14:13:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
      			<p>Cisco icons: types and descriptions</p>
      		</div>
      		<div class = "menuItem">
-			<a style="float:left;"; class = "linker" href="php/Router.php" id = "about">Routers</a>
-			<a style="float:left;"; class = "linker1" href="php/Switch.php" id = "Switch">Switches</a>
+			<a style="float:left;"; class = "linker" href="php/router.php" id = "about">Routers</a>
+			<a style="float:left;"; class = "linker1" href="php/switch.php" id = "Switch">Switches</a>
 			<a style="float:left;"; class = "linker2" href="php/connectors.php" id = "connectors">Connections and links</a>
 			<a style="float:left;"; class = "linker3" href="php/endDs.php" id = "endDevices" href="">End devices</a>
 			<a style="float:left;"; class = "linker4" href="php/other.php" id = "other" href="">Other devices</a>
